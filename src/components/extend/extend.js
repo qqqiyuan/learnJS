01d603a6ccccec470da941c4aa27f0e9e37e3657
miqi <miqi@showjoy.com>
2017-11-30 21:33:40 +0800

    deep extend

--- src/components/extend/extend.js
+++ src/components/extend/extend.js
@@ -1,16 +1,35 @@
 // shadow copy
+// function extend() {
+//   var args = arguments;
+//   var target = args[0];
+//   var i = 1;
+//   for (i; i < args.length; i++) {
+//     for (var k in args[i]) {
+//       target[k] = args[i][k];
+//     }
+//   }
+//   return target;
+// }
+
+// deep copy
 function extend() {
   var args = arguments;
-  var target = args[0];
-  var i = 1;
+  var deep = typeof args[0] === 'boolean' ? args[0] : false;
+  var target = deep ? args[1] : args[0];
+  var i = deep ? 1 : 0;
   for (i; i < args.length; i++) {
-    for (var k in args[i]) {
-      target[k] = args[i][k];
+    var options = args[i];
+    if (options != null) {
+      for (var name in options) {
+        if (deep && target[name] && typeof options[name] === 'object') {
+          extend(deep, target[name], options[name]);
+        } else {
+          target[name] = options[name];
+        }
+      }
     }
   }
   return target;
 }
-
-// deep copy
 
 export default extend;
--- src/components/extend/test.js
+++ src/components/extend/test.js
@@ -8,6 +8,11 @@ var obj1 = {
   c: {
     name: 'wangcai',
     age: '1.6',
+    other: {
+      mail: '11.com',
+      sex: 'male',
+      address: '12121',
+    },
   },
 };
 var obj2 = {
@@ -15,10 +20,14 @@ var obj2 = {
     name: 'dada',
     age: '23',
   },
-  b: {
+  c: {
     name: 'miqi',
     age: '25',
+    other: {
+      sex: 'female',
+      address: '12121',
+    },
   },
 };
 
-console.log(extend(obj1, obj2));
+console.log(extend(true, obj1, obj2));
